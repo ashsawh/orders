4755fb720fc8a99b580ef5b3271ea62965d68a23
Ashwin Sawh <ashwin@sawh.com>
2016-06-30 16:00:57 -0400

    Added comments

--- src/AmountTrait.php
+++ src/AmountTrait.php
@@ -1,13 +1,26 @@
 <?php
-
+/**
+* Mixin containing shared functionality for both payment and items.
+* I thought it would be better to use a trait instead of an
+* abstract class given that the fundamental difference between
+* the two interfaces. As functionality grew, Payments and Items 
+* would deviate more and more.
+*/
 trait AmountTrait {
 	protected $amount; 
+        /**
+        * Get saved amount
+        * @return Int 
+        */
+        public function getAmount() 
+        {
+              return $this->amount;
+      }
 
-	public function getAmount() 
-	{
-		return $this->amount;
-	}
-
+        /**
+        * Standard setter. Sets the amount as long as isValidAmount assetion passes
+        * @param $amount Int
+        */
         public function setAmount($amount)
         {
                 try
@@ -19,6 +32,10 @@ trait AmountTrait {
                 }
         }
 
+        /**
+        * Assertion that amount is an integer. If false exception is thrown and caught 
+        * in setAmount
+        */
         protected function isValidAmount($amount) {
                 if(!is_integer($amount))
                 {
@@ -30,3 +47,4 @@ trait AmountTrait {
                 }
         }
 }
+
--- src/OrderGateway.php
+++ src/OrderGateway.php
@@ -3,22 +3,40 @@
 use Src\Exceptions as Exceptions;
 use Lib\Contracts as Contracts;
 
+/**
+* Orders implements order interface and asserts that user has paid for their item in full
+*
+*/
 class OrderGateway implements Contracts\OrderInterface {
         private $item;
         private $payment;
         
+        /**
+        * Adds item that confirms to ItemInterface
+        * @param $item Lib\Contracts\ItemInterface
+        * @return OrderGateway
+        */
         public function addItem(Contracts\ItemInterface $item) 
         {
             $this->item = $item;
-	    return $this;
+	        return $this;
         }
         
+        /**
+        * Adds payment object that confirms to PaymentInterface
+        * @param $payment Lib\Contracts\PaymentInterface
+        * @return OrderGateway
+        */
         public function addPayment(Contracts\PaymentInterface $payment) 
         {
             $this->payment = $payment;
-	    return $this;
+	       return $this;
         }
 
+        /**
+        * Asserts that a Payment has been set or throw exception
+        *
+        */
         private function hasPayment()
         {
                 if(empty($this->payment))
@@ -27,6 +45,10 @@ class OrderGateway implements Contracts\OrderInterface {
                 }
         }
 
+        /**
+        * Asserts that an item has been set or throw exception
+        *
+        */
         private function hasItem()
         {
                 if(empty($this->payment))
@@ -35,6 +57,10 @@ class OrderGateway implements Contracts\OrderInterface {
                 }
         }
 
+        /**
+        * Asserts that Payment is more than or equal to item amount
+        *
+        */
         public function isPaidInFull()
         {
 	
@@ -49,3 +75,4 @@ class OrderGateway implements Contracts\OrderInterface {
 
         }
 }
+
